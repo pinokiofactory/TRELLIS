2be58e638a6849a751539eb05dabc8b4069682ce
Death <113825692+Deathdadev@users.noreply.github.com>
2024-12-09 13:11:06 +1000

    Update install.js
    Remove hard-coded imports, and clean up code

--- install.js
+++ install.js
@@ -1,6 +1,5 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -17,26 +16,26 @@ module.exports = {
         params: {
           venv: "env",                // Edit this to customize the venv folder path
           path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+          xformers: true   // uncomment this line if your project requires xformers
         }
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
-          "pip install -r requirements.txt"
-        ]
+          "pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless scipy ninja rembg onnxruntime trimesh xatlas pyvista pymeshfix igraph transformers",
+          "pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8",
+          "pip install devicetorch gradio==4.44.1 gradio_litmodel3d==0.0.1"
+          // "pip install -r requirements.txt" // Commented out as the repository does not have a requirements.txt
+        ],
+        venv: "env"
       }
     },
     {
-      method: "fs.link",
+      method: "log",
       params: {
-        venv: "app/env"
+        raw: "Setup process completed successfully!"
       }
     }
   ]
