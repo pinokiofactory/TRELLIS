eac7402c2754c5d2ac841990706b89743549c122
Death <113825692+Deathdadev@users.noreply.github.com>
2024-12-10 22:42:46 +1000

    Update torch.js
    Removed reinstallation of ninja, which has now been installed with conda
    Added triton installation to fix errors with missing triton package on windows

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           "python -m pip install --upgrade pip",
-          "pip install --upgrade setuptools wheel ninja"
+          "pip install --upgrade setuptools wheel"
         ]
       }
     },
@@ -21,6 +21,7 @@ module.exports = {
         "message": [
           "pip install torch==2.5.1 torchvision==0.20.1 {{args && args.xformers ? 'xformers' : ''}}  --index-url https://download.pytorch.org/whl/cu124",
           "pip install https://github.com/bdashore3/flash-attention/releases/download/v2.7.1.post1/flash_attn-2.7.1.post1+cu124torch2.5.1cxx11abiFALSE-cp310-cp310-win_amd64.whl",
+          "pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post5/triton-3.1.0-cp310-cp310-win_amd64.whl",
           // "pip install flash-attn",
           "pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu124.html",
           "pip install git+https://github.com/NVlabs/nvdiffrast.git",
