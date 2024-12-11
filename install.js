c6558caf85c2cb47b1c88d9315f3a93e3401ad17
Death <113825692+Deathdadev@users.noreply.github.com>
2024-12-12 04:44:30 +1000

    Update install.js

--- install.js
+++ install.js
@@ -4,28 +4,8 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "conda install -y conda-forge::ninja",
-        ]
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        message: [
           "git clone --recurse-submodules https://github.com/microsoft/TRELLIS.git app",
         ]
-      }
-    },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          xformers: true   // uncomment this line if your project requires xformers
-        }
       }
     },
     {
@@ -34,18 +14,30 @@ module.exports = {
         venv: "env",
         path: "app",
         message: [
-          "pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless scipy rembg onnxruntime trimesh xatlas pyvista pymeshfix igraph transformers",
-          "pip install git+https://github.com/EasternJournalist/utils3d.git@9a4eb15e4021b67b12c460c7057d642626897ec8",
-          "pip install gradio gradio_litmodel3d==0.0.1"
-          // "pip install -r requirements.txt" // Commented out as the repository does not have a requirements.txt
+          "pip install -U xformers torch==2.5.1 torchvision --index-url https://download.pytorch.org/whl/cu124",
+          "pip install -r ../requirements.txt",
+          "pip install huggingface_hub hf_transfer",
+          "pip install -U setuptools wheel ninja",
+          "pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post5/triton-3.1.0-cp310-cp310-win_amd64.whl"
         ]
       }
     },
     {
-      method: "log",
+      method: "shell.run",
       params: {
-        raw: "Setup process completed successfully!"
+        venv: "env",
+        path: "app",                 
+        message: [
+          "pip install git+https://github.com/JeffreyXiang/diffoctreerast.git",
+          "pip install git+https://github.com/sdbds/diff-gaussian-rasterization"
+        ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"             
       }
     }
   ]
-}
+};
