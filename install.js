21b4d7dfad2aa7259b85869ee9bf2aacf53b3df2
Death <113825692+Deathdadev@users.noreply.github.com>
2024-12-12 05:44:32 +1000

    Update install.js

--- install.js
+++ install.js
@@ -14,11 +14,11 @@ module.exports = {
         venv: "env",
         path: "app",
         message: [
-          "pip install -U xformers torch==2.5.1 torchvision --index-url https://download.pytorch.org/whl/cu124",
-          "pip install -r ../requirements.txt",
-          "pip install huggingface_hub hf_transfer",
-          "pip install -U setuptools wheel ninja",
-          "pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post5/triton-3.1.0-cp310-cp310-win_amd64.whl"
+          "uv pip install -U xformers torch==2.5.1 torchvision --index-url https://download.pytorch.org/whl/cu124",
+          "uv pip install -r ../requirements.txt",
+          "uv pip install huggingface_hub hf_transfer",
+          "uv pip install -U setuptools wheel ninja",
+          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post5/triton-3.1.0-cp310-cp310-win_amd64.whl"
         ]
       }
     },
@@ -28,8 +28,8 @@ module.exports = {
         venv: "env",
         path: "app",                 
         message: [
-          "pip install git+https://github.com/JeffreyXiang/diffoctreerast.git --no-build-isolation",
-          "pip install git+https://github.com/sdbds/diff-gaussian-rasterization --no-build-isolation"
+          "uv pip install git+https://github.com/JeffreyXiang/diffoctreerast.git --no-build-isolation",
+          "uv pip install git+https://github.com/sdbds/diff-gaussian-rasterization --no-build-isolation"
         ]
       }
     },
