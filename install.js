15c41003341b37e0efa959f56c6b9d42e4bcd04b
Death <deathdev06@gmail.com>
2025-01-03 15:59:50 +1000

    Refactor install.js and requirements.txt to streamline package installations

--- install.js
+++ install.js
@@ -17,19 +17,7 @@ module.exports = {
           "uv pip install -U setuptools wheel ninja",
           "uv pip install -U xformers torch==2.5.1 torchvision --index-url https://download.pytorch.org/whl/cu124",
           "uv pip install -r ../requirements.txt",
-          "uv pip install huggingface_hub hf_transfer",
-          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.1.0-windows.post5/triton-3.1.0-cp310-cp310-win_amd64.whl"
-        ]
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",
-        path: "app",                 
-        message: [
-          "uv pip install https://github.com/iiiytn1k/sd-webui-some-stuff/releases/download/diffoctreerast/diffoctreerast-0.0.0-cp310-cp310-win_amd64.whl", // "uv pip install git+https://github.com/JeffreyXiang/diffoctreerast.git --no-build-isolation",
-          "uv pip install https://github.com/sdbds/diff-gaussian-rasterization/releases/download/diff-gaussian-rasterization/diff_gaussian_rasterization-0.0.0-cp310-cp310-win_amd64.whl", // "uv pip install git+https://github.com/sdbds/diff-gaussian-rasterization --no-build-isolation"
+          "uv pip install huggingface_hub hf_transfer"
         ]
       }
     },
