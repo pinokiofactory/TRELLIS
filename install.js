79498ff4ba8fea0fa8b84553a4a533f105a39824
Death <113825692+Deathdadev@users.noreply.github.com>
2025-02-08 22:43:06 +1000

    Add files via upload

--- install.js
+++ install.js
@@ -10,28 +10,30 @@ module.exports = {
       }
     },
     // Delete this step if your project does not use torch
-    // {
-    //   method: "script.start",
-    //   params: {
-    //     uri: "torch.js",
-    //     params: {
-    //       venv: "env",                // Edit this to customize the venv folder path
-    //       path: "app",                // Edit this to customize the path to start the shell from
-    //       xformers: true   // uncomment this line if your project requires xformers
-    //     }
-    //   }
-    // },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
         env: {
           "UV_DEFAULT_INDEX": "https://pypi.org/simple",
-          "UV_INDEX": "https://download.pytorch.org/whl/cu121 https://download.pytorch.org/whl/cu124",
-          "UV_FIND_LINKS": "https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu121.html",
+          "UV_INDEX": "https://download.pytorch.org/whl/cu124",
+          // "UV_FIND_LINKS": "https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu124.html",
           "UV_INDEX_STRATEGY": "unsafe-best-match",
           "UV_NO_BUILD_ISOLATION": 1,
-          "UV_LINK_MODE": "symlink"
+          "UV_LINK_MODE": "symlink",
+          "USE_NINJA": 0,
+          "DISTUTILS_USE_SDK": 1
         },
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
